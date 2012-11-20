ed0f2083708ed8baa4bc79d6acb913270144ba28
Kuniyoshi Kouji <kuniyoshi@cpan.com>
2012-11-21 02:34:02 +0900

    update cache mech.

--- webapp/perl/service/gracer.d/grace.pl
+++ webapp/perl/service/gracer.d/grace.pl
@@ -7,9 +7,11 @@ use open qw( :utf8 :std );
 use Data::Dumper;
 use Time::HiRes qw( sleep gettimeofday tv_interval );
 use lib "/home/isucon/isucon2/webapp/perl/extlib/lib/perl5";
+use Parallel::ForkManager;
 use Furl;
 
 my $MAX_INTERVAL = 0.90;
+my $MAX_REQUESTS = 100;
 my @URLS         = (
     "http://192.168.1.121/recent_sold",
     "http://192.168.1.121/",
@@ -22,24 +24,33 @@ my @URLS         = (
     "http://192.168.1.121/ticket/5",
 );
 
+my $pm = Parallel::ForkManager->new( scalar @URLS );
 my $ua = Furl->new;
 
-while ( 1 ) {
-    my $start = [ gettimeofday ];
+warn "### pm renew";
 
-    foreach my $url ( @URLS ) {
-        my $res = $ua->request(
-            method => "GRACE",
-            url    => $url,
+foreach my $url ( @URLS ) {
+    my $child = $pm->start
+        and next;
+    my $count;
+
+    while ( $count++ < $MAX_REQUESTS ) {
+        my $start = [ gettimeofday ];
+        my $res   = $ua->request(
+            method  => "GRACE",
+            url     => $url,
+            headers => [ "Connection" => "close" ],
         );
 
         warn "GRACE $url";
-    }
 
-    my $interval = $MAX_INTERVAL - tv_interval( $start );
+        my $interval = $MAX_INTERVAL - tv_interval( $start );
 
-    if ( $interval > 0 ) {
-        sleep $interval;
-        warn "slept: ${interval}s";
+        if ( $interval > 0 ) {
+            sleep $interval;
+            warn "slept: ${interval}s after $url";
+        }
     }
+    $pm->finish;
 }
+$pm->wait_all_children;
