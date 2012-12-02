52b243793622fd555b0c189c391e4b62ec65cc05
Kuniyoshi Kouji <kuniyoshi@cpan.comnexport>
2012-12-02 05:28:21 -0500

    use base template for all views.

--- webapp/erlang/src/isucon2_app.erl
+++ webapp/erlang/src/isucon2_app.erl
@@ -4,7 +4,7 @@
 
 start(_StartType, _StartArgs) ->
     Dispatch = [{'_', [
-                       {['_'], isucon2_index, []}]}],
+                       {[], isucon2_index, []}]}],
     {ok, _} = cowboy:start_http(http, 100, [{port, 5000}], [{dispatch, Dispatch}]),
     isucon2_sup:start_link().
 
--- webapp/erlang/src/isucon2_index.erl
+++ webapp/erlang/src/isucon2_index.erl
@@ -7,8 +7,11 @@ init(_Transport, Req, []) ->
     {ok, Req, undefined}.
 
 handle(Req, State) ->
-    {ok, Body} = isucon2_index_dtl:render([{homepage, foobar}]),
-    {ok, Req2} = cowboy_req:reply(200, [], Body, Req),
+    {ok, Content}  = isucon2_index_dtl:render([{homepage, foobar}]),
+%%    io:format("content: ~p~n", [Content]),
+    {ok, Body} = isucon2_base_dtl:render([{content, Content}]),
+%%    io:format("body: ~p~n", [Body]),
+    {ok, Req2}  = cowboy_req:reply(200, [], Body, Req),
     {ok, Req2, State}.
 
 terminate(_Req, _State) ->
